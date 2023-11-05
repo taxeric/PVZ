1e2d513bc8f856c192d650fe21cbd5322a2f6d18
幻弦让叶 <1248389474@qq.com>
2023-11-05 21:25:35 +0800

    新增阳光生成间隔属性

--- models/plants/Sunflower.h
+++ models/plants/Sunflower.h
@@ -9,7 +9,20 @@
 
 class Sunflower: public Plant {
 
+private:
+
+    /**
+     * 阳光生成间隔
+     */
+    int createFre;
+
 public:
+
+    /**
+     * 当前间隔计次
+     */
+    int fre;
+
     Sunflower(std::string cardPath, std::string actionPath, int actionPicCount, int index) {
         this->setName("向日葵");
         this->setCardPath(cardPath);
@@ -20,6 +33,8 @@ public:
         this->hp = 100;
         this->sunshine = 50;
         this->actionPicCount = actionPicCount;
+        this->fre = 0;
+        this->createFre = 1000;
     }
 
     explicit Sunflower(Plant *plant) {
@@ -31,6 +46,8 @@ public:
         column = temp->column;
         sunshine = temp->sunshine;
         actionPicCount = temp->actionPicCount;
+        fre = temp->fre;
+        createFre = temp->createFre;
         setName(temp->getName());
         setCoolDown(temp->getCoolDown());
         std::cout << "event: [new] " << temp->getName() << std::endl;
@@ -43,6 +60,10 @@ public:
     }
 
     ~Sunflower() override= default;
+
+    int getCreateFre() const {
+        return createFre;
+    }
 };
 
 #endif //PVZ_SUNFLOWER_H
