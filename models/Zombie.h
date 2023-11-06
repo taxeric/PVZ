5aab71304fbe14fb85f3dddff271ce07eead085b
幻弦让叶 <1248389474@qq.com>
2023-11-06 17:11:13 +0800

    新增僵尸状态🧟‍♂️

--- models/Zombie.h
+++ models/Zombie.h
@@ -13,11 +13,34 @@ struct Zombie {
     int speed;
     int hp;
     bool eating;
-    bool freeze;
-    int freezeTimer;
     bool head;
     bool lostHead;
     bool dead;
+
+    /**
+     * 切图时是否等待, 用于僵尸移动完善
+     */
+    bool isWait;
+
+    /**
+     * 是否已经播放过冻结音效
+     */
+    bool freezeSoundPlayed;
+
+    /**
+     * 是否冻结
+     */
+    bool freeze;
+
+    /**
+     * 冻结状态下行走计次, 经过n轮后开始行走
+     */
+    int freezeWalkActionTimer;
+
+    /**
+     * 冻结计次, 如果收到寒冰设计伤害经过了n轮计次则回到正常移动状态
+     */
+    int freezeTimer;
 
     /**
      * 是否低吼过
